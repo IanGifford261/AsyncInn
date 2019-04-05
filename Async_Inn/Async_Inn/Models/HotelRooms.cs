a719dfb8fd8244f4072c54d41c0cd1d1e6a80622
iangifford <lightyagamie10@gmail.com>
2019-04-04 20:39:55 -0700

    HotelRooms

--- Async_Inn/Async_Inn/Models/HotelRooms.cs
+++ Async_Inn/Async_Inn/Models/HotelRooms.cs
@@ -7,5 +7,13 @@ namespace Async_Inn.Models
 {
     public class HotelRooms
     {
+        public int HotelsID { get; set; }
+        public int RoomNumber { get; set; }
+        public int RoomsID { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+
+        public Hotels Hotels { get; set; }
+        public Rooms Rooms { get; set; }
     }
 }
