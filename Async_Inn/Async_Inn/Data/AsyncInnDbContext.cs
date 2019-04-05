d09b8dc285b1c9285f0b2c8869b197f64fd2ed76
iangifford <lightyagamie10@gmail.com>
2019-04-04 20:26:43 -0700

    DB build to spec for branch due time

--- Async_Inn/Async_Inn/Data/AsyncInnDbContext.cs
+++ Async_Inn/Async_Inn/Data/AsyncInnDbContext.cs
@@ -9,9 +9,22 @@ namespace Async_Inn.Data
 {
     public class AsyncInnDbContext : DbContext
     {
-        public Async_InnDbContext(DbContextOptions<Async_InnDbContext> options) : base(options)
+        public AsyncInnDbContext(DbContextOptions<AsyncInnDbContext> options) : base(options)
         {
 
         }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HotelRooms>().HasKey(ce => new { ce.HotelID, RoomsID });
+            modelBuilder.Entity<RoomAmenities>().HasKey(ce => new { ce.RoomsID, AmenitiesID });
+        }
+
+
+        public DbSet<Hotels> Hotels {get; set;}
+        public DbSet<HotelRooms> HotelRooms { get; set; }
+        public DbSet<Rooms> Rooms { get; set; }
+        public DbSet<RoomAmenities> RoomAmenities { get; set; }
+        public DbSet<Amenities> Amenities { get; set; }
     }
 }
