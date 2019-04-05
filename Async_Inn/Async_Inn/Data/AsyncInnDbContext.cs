2e1693f1d42f96700f1f3b37788324ee316a1d3b
iangifford <lightyagamie10@gmail.com>
2019-04-04 20:02:05 -0700

    building DB and Models

---
+++ Async_Inn/Async_Inn/Data/AsyncInnDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Async_Inn.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Data
+{
+    public class AsyncInnDbContext : DbContext
+    {
+        public Async_InnDbContext(DbContextOptions<Async_InnDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
