a4b1f7da7c72ee9e98bb09cc77f895f1b886ab56
iangifford <lightyagamie10@gmail.com>
2019-04-09 11:33:36 -0700

    Adding Interfaces/services and tasks

---
+++ Async_Inn/Async_Inn/Models/Services/AmenitiesService.cs
@@ -0,0 +1,14 @@
+﻿using Async_Inn.Data;
+using Async_Inn.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Async_Inn.Models.Services
+{
+    public class AmenitiesService
+    {
+    }
+}
